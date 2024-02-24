2f36a8498a9c7bedc4586fc98ad4172bf3c976a7
Ivan Fish <vania.fish@mail.ru>
2024-02-24 09:44:45 +0300

    first commit

--- Factory/CalculableFactory.java
+++ Factory/CalculableFactory.java
@@ -6,7 +6,7 @@ import ResultWorkJava.Model.ComplexNumber;
 import Seminar4.Calculator;
 
 public class CalculableFactory implements ICalculableFactory {
-    publicS Calculable create(ComplexNumber primaryArg){
+    public Calculable create(ComplexNumber primaryArg){
         return new ComplexCalculator(primaryArg);
     }
 }
